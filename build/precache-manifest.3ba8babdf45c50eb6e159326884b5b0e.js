59c58da8c5ed455aaf578bb94f5c9e8aa57bcaff
aisukdl <51831489+aisukdl@users.noreply.github.com>
2020-01-31 14:31:01 +0700

    Add files via upload

---
+++ build/precache-manifest.3ba8babdf45c50eb6e159326884b5b0e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cadafd271d681caf4727054a1530616c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f7f2129790f8e77faa80",
+    "url": "/static/css/main.cb8380da.chunk.css"
+  },
+  {
+    "revision": "a31fb27fa4c80e849568",
+    "url": "/static/js/2.5e7fe5d5.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.5e7fe5d5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f7f2129790f8e77faa80",
+    "url": "/static/js/main.865a6fc7.chunk.js"
+  },
+  {
+    "revision": "4e81e6700ec56eb4ae23",
+    "url": "/static/js/runtime-main.5eb8e6b3.js"
+  },
+  {
+    "revision": "ed77873c5a78f67759f211de61835276",
+    "url": "/static/media/pic.ed77873c.jpg"
+  }
+]);
